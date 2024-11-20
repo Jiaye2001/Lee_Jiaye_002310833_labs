62b60f94afe5061899d0ec6424e9c1f170781ebd
Jiaye Lee <lee.jiay@northeastern.edu>
2024-11-19 20:04:58 -0500

    Lab8 - Q4 completed

--- Lab 8/src/main/SocialMedia_Main.java
+++ Lab 8/src/main/SocialMedia_Main.java
@@ -94,8 +94,9 @@ public class SocialMedia_Main {
     
     private void runAnalysis(){
         // TODO
-        helper.getAverageLikesPerComments();  // Q1
+        helper.getAverageLikesPerComments(); // Q1
         helper.getMaxLikeCommentPost(); // Q2
         helper.getPostWithMostComments(); // Q3
+        helper.getPassiveUsers(); //Q4
     }
 }
